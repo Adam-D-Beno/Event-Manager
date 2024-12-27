2e8a237e68a3f8ec71e0559805c3acb5c24c7fca
ПК_Home <wrofdt@ya.ru>
2024-12-27 19:47:08 +0300

    Create test class

---
+++ src/test/java/org/das/event_manager/EventManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.das.event_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

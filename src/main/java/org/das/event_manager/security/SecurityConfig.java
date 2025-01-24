8944a77252c5e2174ed000c7be6cd4782f920ae6
NoutVivo <fst8822@gmail.com>
2025-01-24 19:35:57 +0300

    refactor SecurityConfig

--- src/main/java/org/das/event_manager/security/SecurityConfig.java
+++ src/main/java/org/das/event_manager/security/SecurityConfig.java
@@ -20,6 +20,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.AnonymousAuthenticationFilter;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableMethodSecurity
@@ -95,7 +96,7 @@ public class SecurityConfig {
                                 exception.authenticationEntryPoint(authenticationEntryPoint);
                                 exception.accessDeniedHandler(customAccessDeniedHandler);
                 })
-                .addFilterBefore(jwtTokenFilter, AnonymousAuthenticationFilter.class)
+                .addFilterBefore(jwtTokenFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
 

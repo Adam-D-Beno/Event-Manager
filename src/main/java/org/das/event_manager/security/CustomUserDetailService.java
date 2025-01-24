809a913757f8ed1ad0370996644bf2f787d1dd29
ПК_Home <wrofdt@ya.ru>
2025-01-24 11:44:42 +0300

    Refactor CustomUserDetailService

--- src/main/java/org/das/event_manager/security/CustomUserDetailService.java
+++ src/main/java/org/das/event_manager/security/CustomUserDetailService.java
@@ -10,6 +10,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Component;
 import java.util.Collections;
+import java.util.List;
 
 @Component
 public class CustomUserDetailService implements UserDetailsService {
@@ -28,7 +29,7 @@ public class CustomUserDetailService implements UserDetailsService {
                 .map(user -> new User(
                         user.getLogin(),
                         user.getPassword(),
-                        Collections.singleton(new SimpleGrantedAuthority(user.getRole()))
+                        List.of(new SimpleGrantedAuthority(user.getRole()))
                 ))
                 .orElseThrow(() -> new UsernameNotFoundException("User not found with user login: = %s"
                         .formatted(login)));

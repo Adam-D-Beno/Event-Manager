1ba6b8de94f57a80b8127606e64d4684090262ed
ПК_Home <wrofdt@ya.ru>
2025-01-24 17:02:12 +0300

    Refactor UserDto

--- src/main/java/org/das/event_manager/dto/UserDto.java
+++ src/main/java/org/das/event_manager/dto/UserDto.java
@@ -1,9 +1,11 @@
 package org.das.event_manager.dto;
 
+import org.das.event_manager.utils.Role;
+
 public record UserDto(
         Long id,
         String login,
         Integer age,
-        String role
+        Role role
 ) {
 }

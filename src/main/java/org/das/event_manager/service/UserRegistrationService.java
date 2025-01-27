ed11a06474f9fe524abfe244672e7d9c9776b219
ПК_Home <wrofdt@ya.ru>
2025-01-27 07:52:34 +0300

    create UserRegistrationService

---
+++ src/main/java/org/das/event_manager/service/UserRegistrationService.java
@@ -0,0 +1,39 @@
+package org.das.event_manager.service;
+
+import org.das.event_manager.domain.User;
+import org.das.event_manager.domain.UserRole;
+import org.das.event_manager.domain.entity.UserEntity;
+import org.das.event_manager.mappers.UserEntityMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserRegistrationService {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(UserRegistrationService.class);
+    private final UserService userService;
+    private final PasswordEncoder passwordEncoder;
+
+    public UserRegistrationService(
+            @Lazy UserService userService,
+            PasswordEncoder passwordEncoder
+    ) {
+        this.userService = userService;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    public User register(User signUpRequest) {
+        LOGGER.info("Execute method register user: login = {} in UserRegistrationService class", signUpRequest.login());
+        userService.userExistByLogin(signUpRequest.login());
+        return new User(
+                null,
+                signUpRequest.login(),
+                passwordEncoder.encode(signUpRequest.passwordHash()),
+                signUpRequest.age(),
+                UserRole.USER
+        );
+    }
+}

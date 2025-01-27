5f49f1bd530942d70c6df740a102443bc31ec6d4
ПК_Home <wrofdt@ya.ru>
2025-01-27 07:52:20 +0300

    refactor UserService

--- src/main/java/org/das/event_manager/service/UserService.java
+++ src/main/java/org/das/event_manager/service/UserService.java
@@ -4,11 +4,9 @@ import org.das.event_manager.domain.User;
 import org.das.event_manager.domain.entity.UserEntity;
 import org.das.event_manager.mappers.UserEntityMapper;
 import org.das.event_manager.repository.UserRepository;
-import org.das.event_manager.domain.UserRole;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -17,29 +15,31 @@ public class UserService {
     private static final Logger LOGGER = LoggerFactory.getLogger(UserService.class);
     private final UserRepository userRepository;
     private final UserEntityMapper userEntityMapper;
-    private final PasswordEncoder passwordEncoder;
+    private final UserRegistrationService userRegistrationService;
 
     public UserService(
             UserRepository userRepository,
-                       UserEntityMapper userEntityMapper,
-                       PasswordEncoder passwordEncoder
+            UserEntityMapper userEntityMapper,
+            UserRegistrationService userRegistrationService
     ) {
         this.userRepository = userRepository;
         this.userEntityMapper = userEntityMapper;
-        this.passwordEncoder = passwordEncoder;
+        this.userRegistrationService = userRegistrationService;
     }
 
-    public User register(User signUpRequest) {
+
+    public User save(User signUpRequest) {
         LOGGER.info("Execute method register user: login = {} in UserService class", signUpRequest.login());
-        if (userRepository.existsByLogin(signUpRequest.login())) {
+        User registered = userRegistrationService.register(signUpRequest);
+        UserEntity userToSave = userEntityMapper.toEntity(registered);
+        UserEntity saved = userRepository.save(userToSave);
+        return userEntityMapper.toDomain(saved);
+    }
+
+    public void userExistByLogin(String login) {
+        if (userRepository.existsByLogin(login)) {
             throw new IllegalArgumentException("User already taken");
         }
-        var hashedPass = passwordEncoder.encode(signUpRequest.passwordHash());
-        UserEntity userToSave = userEntityMapper.toEntity(signUpRequest);
-        userToSave.setPassword(hashedPass);
-        userToSave.setRole(UserRole.USER);
-        UserEntity userSaved = userRepository.save(userToSave);
-        return userEntityMapper.toDomain(userSaved);
     }
 
     public User findByLogin(String login) {

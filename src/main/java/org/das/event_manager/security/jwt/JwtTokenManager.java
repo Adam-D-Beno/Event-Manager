520510977f3a02ab9b519207785186c17eddfee3
ПК_Home <wrofdt@ya.ru>
2025-01-28 23:50:52 +0300

    refactor JwtTokenManager

--- src/main/java/org/das/event_manager/security/jwt/JwtTokenManager.java
+++ src/main/java/org/das/event_manager/security/jwt/JwtTokenManager.java
@@ -1,7 +1,10 @@
 package org.das.event_manager.security.jwt;
 
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
+import org.das.event_manager.domain.User;
+import org.das.event_manager.domain.UserRole;
 import org.das.event_manager.service.AuthenticationService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -9,29 +12,41 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import javax.management.relation.Role;
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 @Component
 public class JwtTokenManager {
     private static final Logger LOGGER = LoggerFactory.getLogger(JwtTokenManager.class);
-    private final SecretKey SECRET_KEY;
-    public final long EXPIRATION_TIME;
+    private final Key secretKey;
+    public final long expirationTime;
 
     public JwtTokenManager(
             @Value("${jwt.sign_key}") String secretKey,
             @Value("${jwt.live_time}") long expirationTime) {
-        SECRET_KEY = Keys.hmacShaKeyFor(secretKey.getBytes());
-        EXPIRATION_TIME = expirationTime;
+        this.secretKey = new SecretKeySpec(
+                secretKey.getBytes(StandardCharsets.UTF_8),
+                SignatureAlgorithm.HS256.getJcaName()
+        );
+        this.expirationTime = expirationTime;
     }
 
-    public String generateJwtToken(String login) {
-        LOGGER.info("Generate jwt token for login = {}", login);
+    public String generateJwtToken(User user) {
+        LOGGER.info("Generate jwt token for login = {}", user.login());
+        Map<String, Object> claims = new HashMap<>();
+        claims.put("role", user.userRole().name());
         return Jwts
                 .builder()
-                .subject(login)
-                .signWith(SECRET_KEY)
+                .claims(claims)
+                .subject(user.login())
+                .signWith(secretKey)
                 .issuedAt(new Date())
-                .expiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
+                .expiration(new Date(System.currentTimeMillis() + expirationTime))
                 .compact();
     }
 
@@ -39,10 +54,21 @@ public class JwtTokenManager {
         LOGGER.info("Get login from jwt token = {} ", jwt);
         return Jwts
                 .parser()
-                .verifyWith(SECRET_KEY)
+                .setSigningKey(secretKey)
                 .build()
-                .parseSignedClaims(jwt)
+                .parseClaimsJws(jwt)
                 .getPayload()
                 .getSubject();
+    }
+
+    public String getRoleFromToken(String jwt) {
+        LOGGER.info("Get role from jwt token = {} ", jwt);
+        return Jwts
+                .parser()
+                .setSigningKey(secretKey)
+                .build()
+                .parseClaimsJws(jwt)
+                .getPayload()
+                .get("role", String.class);
     }
 }

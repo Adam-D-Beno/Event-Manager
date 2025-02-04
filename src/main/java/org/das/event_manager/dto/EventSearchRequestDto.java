a9e6a667b92e6f14ae16a9f7bb5b9ee76a79ad39
ПК_Home <wrofdt@ya.ru>
2025-02-04 22:28:29 +0300

    create EventSearchRequestDto

---
+++ src/main/java/org/das/event_manager/dto/EventSearchRequestDto.java
@@ -0,0 +1,21 @@
+package org.das.event_manager.dto;
+
+import org.das.event_manager.domain.EventStatus;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+public record EventSearchRequestDto(
+      String name,
+      Integer placesMin,
+      Integer placesMax,
+      LocalDateTime dateStartAfter,
+      LocalDateTime dateStartBefore,
+      BigDecimal costMin,
+      BigDecimal costMax,
+      Integer durationMin,
+      Integer durationMax,
+      Integer locationId,
+      EventStatus status
+) {
+}

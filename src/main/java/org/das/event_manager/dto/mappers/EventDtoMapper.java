d1aa88bb39db96b20751a85717c0e519c3aaa6c6
ПК_Home <wrofdt@ya.ru>
2025-02-09 21:03:33 +0300

    refactor EventDtoMapper

--- src/main/java/org/das/event_manager/dto/mappers/EventDtoMapper.java
+++ src/main/java/org/das/event_manager/dto/mappers/EventDtoMapper.java
@@ -1,6 +1,7 @@
 package org.das.event_manager.dto.mappers;
 
 import org.das.event_manager.domain.Event;
+import org.das.event_manager.domain.EventStatus;
 import org.das.event_manager.domain.Location;
 import org.das.event_manager.dto.EventCreateRequestDto;
 import org.das.event_manager.dto.EventDto;
@@ -18,16 +19,41 @@ public class EventDtoMapper {
     public Event toDomain(EventCreateRequestDto eventCreateRequestDto) {
         LOGGER.info("Execute method to toDomain in EventDtoMapper class eventCreateRequestDto = {}",
                 eventCreateRequestDto);
-        return null;
+        return new Event(
+                null,
+                eventCreateRequestDto.name(),
+                null,
+                eventCreateRequestDto.maxPlaces(),
+                null,
+                eventCreateRequestDto.date(),
+                eventCreateRequestDto.cost(),
+                eventCreateRequestDto.duration(),
+                eventCreateRequestDto.locationId(),
+                EventStatus.WAIT_START
+
+        );
     }
 
     public EventDto toDto(Event event) {
         LOGGER.info("Execute method to toDto in EventDtoMapper class,  event = {}", event);
-        return null;
+        return new EventDto(
+                event.id(),
+                event.name(),
+                event.ownerId(),
+                event.maxPlaces(),
+                event.occupiedPlaces(),
+                event.date(),
+                event.cost(),
+                event.duration(),
+                event.locationId(),
+                event.status()
+        );
     }
 
     public List<EventDto> toDto(List<Event> events) {
         LOGGER.info("Execute method to toDto in EventDtoMapper class, events ={}", events);
-        return null;
+        return events.stream()
+                .map(this::toDto)
+                .toList();
     }
 }

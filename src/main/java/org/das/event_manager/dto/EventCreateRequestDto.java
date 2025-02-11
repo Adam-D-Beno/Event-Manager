d4ee8167936e79120db7498dd58082797e286270
NoutVivo <fst8822@gmail.com>
2025-02-11 21:04:19 +0300

    refactor EventCreateRequestDto

--- src/main/java/org/das/event_manager/dto/EventCreateRequestDto.java
+++ src/main/java/org/das/event_manager/dto/EventCreateRequestDto.java
@@ -4,12 +4,13 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
+import java.time.ZonedDateTime;
 
 public record EventCreateRequestDto(
         String name,
         Integer maxPlaces,
         @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSSXXX")
-        LocalDateTime date,
+        ZonedDateTime date,
         BigDecimal cost,
         Integer duration,
         Long locationId

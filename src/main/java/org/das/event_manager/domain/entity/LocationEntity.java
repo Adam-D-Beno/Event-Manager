8657c4c95dad9ae8e5dc492cdc9045e77dd14e85
NoutVivo <fst8822@gmail.com>
2025-02-11 07:47:54 +0300

    refactor LocationEntity

--- src/main/java/org/das/event_manager/domain/entity/LocationEntity.java
+++ src/main/java/org/das/event_manager/domain/entity/LocationEntity.java
@@ -6,8 +6,6 @@ import lombok.Setter;
 
 @Entity
 @Table(name = "locations")
-@Setter
-@Getter
 public class LocationEntity {
    @Id
    @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -45,5 +43,45 @@ public class LocationEntity {
 
     public LocationEntity() {
 
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public Integer getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(Integer capacity) {
+        this.capacity = capacity;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
     }
 }

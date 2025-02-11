2e6312d6f970c9f8b66e9bdb92264d09d046d068
NoutVivo <fst8822@gmail.com>
2025-02-11 07:47:40 +0300

    refactor EventEntity

--- src/main/java/org/das/event_manager/domain/entity/EventEntity.java
+++ src/main/java/org/das/event_manager/domain/entity/EventEntity.java
@@ -11,8 +11,6 @@ import java.util.List;
 
 @Entity
 @Table(name = "events")
-@Setter
-@Getter
 public class EventEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -77,5 +75,93 @@ public class EventEntity {
         this.duration = duration;
         this.location = location;
         this.status = status;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public UserEntity getOwner() {
+        return owner;
+    }
+
+    public void setOwner(UserEntity owner) {
+        this.owner = owner;
+    }
+
+    public Integer getMaxPlaces() {
+        return maxPlaces;
+    }
+
+    public void setMaxPlaces(Integer maxPlaces) {
+        this.maxPlaces = maxPlaces;
+    }
+
+    public Integer getOccupiedPlaces() {
+        return occupiedPlaces;
+    }
+
+    public void setOccupiedPlaces(Integer occupiedPlaces) {
+        this.occupiedPlaces = occupiedPlaces;
+    }
+
+    public LocalDateTime getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDateTime date) {
+        this.date = date;
+    }
+
+    public BigDecimal getCost() {
+        return cost;
+    }
+
+    public void setCost(BigDecimal cost) {
+        this.cost = cost;
+    }
+
+    public Integer getDuration() {
+        return duration;
+    }
+
+    public void setDuration(Integer duration) {
+        this.duration = duration;
+    }
+
+    public LocationEntity getLocation() {
+        return location;
+    }
+
+    public void setLocation(LocationEntity location) {
+        this.location = location;
+    }
+
+    public EventStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(EventStatus status) {
+        this.status = status;
+    }
+
+    public List<UserRegistration> getMembers() {
+        return members;
+    }
+
+    public void setMembers(List<UserRegistration> members) {
+        this.members = members;
     }
 }

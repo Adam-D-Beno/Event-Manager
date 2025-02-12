9af31f7ca8a6372e28652803f353d5751de33dc3
ПК_Home <wrofdt@ya.ru>
2025-02-12 16:02:18 +0300

    refactor EventService

--- src/main/java/org/das/event_manager/service/EventService.java
+++ src/main/java/org/das/event_manager/service/EventService.java
@@ -13,22 +13,39 @@ public class EventService {
     private static final Logger LOGGER = LoggerFactory.getLogger(EventService.class);
     private final EventRepository eventRepository;
     private final EventEntityMapper eventEntityMapper;
+    private final LocationService locationService;
 
     public EventService(
             EventRepository eventRepository,
-            EventEntityMapper eventEntityMapper
+            EventEntityMapper eventEntityMapper,
+            LocationService locationService
     ) {
         this.eventRepository = eventRepository;
         this.eventEntityMapper = eventEntityMapper;
+        this.locationService = locationService;
     }
 
 
     public Event create(Event event) {
         LOGGER.info("Execute method create in EventService, event = {}", event);
+        checkMaxPlaces(event);
         EventEntity eventEntity = eventEntityMapper.toEntity(event);
         EventEntity saved = eventRepository.save(eventEntity);
         return eventEntityMapper.toDomain(saved);
     }
 
+    public void checkMaxPlaces(Event event) {
+        LOGGER.info("Checking max places for event: {}", event);
+        Integer locationCapacity = locationService.getCapacity(event.locationId());
+        if (locationCapacity == null) {
+            LOGGER.error("Location capacity is null for locationId: {}", event.locationId());
+            throw new IllegalArgumentException("Location capacity cannot be null");
+        }
+        if (event.maxPlaces() > locationCapacity) {
+            LOGGER.error("Error, Maximum number = {} of places at the event more then location capacity = {} ",
+                    event.maxPlaces(), locationCapacity);
+            throw new IllegalArgumentException("maxPlaces more then location maxPlaces");
+        }
+    }
 
 }

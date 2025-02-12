4060409562e431b76508b169627e57252ba7a16e
ПК_Home <wrofdt@ya.ru>
2025-02-12 20:40:29 +0300

    refactor LocationRepository

--- src/main/java/org/das/event_manager/repository/LocationRepository.java
+++ src/main/java/org/das/event_manager/repository/LocationRepository.java
@@ -8,6 +8,8 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Optional;
+
 @Repository
 public interface LocationRepository extends JpaRepository<LocationEntity, Long> {
 
@@ -16,7 +18,7 @@ public interface LocationRepository extends JpaRepository<LocationEntity, Long> 
     boolean existsById(Long id);
 
     @Query("SELECT l.capacity FROM LocationEntity l WHERE l.id = :id")
-    Integer findCapacityById(@Param("id") Long id);
+    Optional<Integer> getCapacityById(@Param("id") Long id);
 
     @Transactional
     @Modifying

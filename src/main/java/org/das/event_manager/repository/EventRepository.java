7015bc01f8f0cbc88e48ad0ccd523a92cc7960a0
NoutVivo <fst8822@gmail.com>
2025-02-17 14:29:41 +0300

    refactor EventRepository

--- src/main/java/org/das/event_manager/repository/EventRepository.java
+++ src/main/java/org/das/event_manager/repository/EventRepository.java
@@ -2,9 +2,16 @@ package org.das.event_manager.repository;
 
 import org.das.event_manager.domain.entity.EventEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public  interface EventRepository extends JpaRepository<EventEntity, Long> {
 
+    @Query
+    List<EventEntity> search(
+
+    );
 }

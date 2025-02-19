6fde7745b7dc0aa37545db55bc46ecf9f43c6e6e
ПК_Home <wrofdt@ya.ru>
2025-02-19 21:18:25 +0300

    refactor RegistrationOnEventService

--- src/main/java/org/das/event_manager/service/RegistrationOnEventService.java
+++ src/main/java/org/das/event_manager/service/RegistrationOnEventService.java
@@ -12,6 +12,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class RegistrationOnEventService {
 

7d743e1d554e5015d12a9010043b27d30d630ea1
ПК_Home <wrofdt@ya.ru>
2025-02-19 21:43:02 +0300

    refactor RegistrationEntityMapper

---
+++ src/main/java/org/das/event_manager/dto/mappers/RegistrationEntityMapper.java
@@ -0,0 +1,27 @@
+package org.das.event_manager.dto.mappers;
+
+import org.das.event_manager.domain.Registration;
+import org.das.event_manager.domain.entity.RegistrationEntity;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import java.util.List;
+
+@Component
+public class RegistrationEntityMapper {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(RegistrationEntityMapper.class);
+
+
+   public Registration toDomain(RegistrationEntity registrationEntity) {
+       LOGGER.info("Execute method toDomain in RegistrationEntityMapper,registrationEntity = {}"
+               , registrationEntity);
+        return null;
+    }
+
+   public List<Registration> toDomain(List<RegistrationEntity> registrationEntities) {
+       LOGGER.info("Execute method toDomain in RegistrationEntityMapper,registrationEntities = {}"
+               , registrationEntities);
+        return null;
+    }
+}

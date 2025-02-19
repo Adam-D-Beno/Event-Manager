dfb5e00178137ad9c042f92deab64efa0a22fa3a
NoutVivo <fst8822@gmail.com>
2025-02-19 13:32:30 +0300

    refactor EventRepository

--- src/main/java/org/das/event_manager/repository/EventRepository.java
+++ src/main/java/org/das/event_manager/repository/EventRepository.java
@@ -2,12 +2,10 @@ package org.das.event_manager.repository;
 
 import org.das.event_manager.domain.EventStatus;
 import org.das.event_manager.domain.entity.EventEntity;
-import org.das.event_manager.dto.EventSearchRequestDto;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.List;
@@ -43,4 +41,6 @@ public  interface EventRepository extends JpaRepository<EventEntity, Long> {
             @Param("locationId") Long  locationId,
             @Param("status") EventStatus status
     );
+
+    List<EventEntity> findByOwner_Id(Long ownerId);
 }

207e02238807be5e654ae2f7e0584a35cf3e5a0e
ПК_Home <wrofdt@ya.ru>
2025-02-25 20:48:49 +0300

    refactor RegistrationEntity

--- src/main/java/org/das/event_manager/domain/entity/RegistrationEntity.java
+++ src/main/java/org/das/event_manager/domain/entity/RegistrationEntity.java
@@ -3,11 +3,11 @@ package org.das.event_manager.domain.entity;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
-
 import java.time.LocalDateTime;
 
 @Entity
-@Table(name = "registrations")
+@Table(name = "registrations", uniqueConstraints =
+            @UniqueConstraint(columnNames = {"user_id", "event_id"}))
 @Setter
 @Getter
 public class RegistrationEntity {
@@ -15,18 +15,21 @@ public class RegistrationEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @ManyToOne
-    @JoinColumn(name = "user_id", nullable = false)
-    private UserEntity user;
-
-    @ManyToOne
+    @ManyToOne()
     @JoinColumn(name = "event_id", nullable = false)
     private EventEntity event;
 
-    @Column(name = "date")
+    @Column(name = "date_registration")
     private LocalDateTime dateRegistration;
 
-    public RegistrationEntity(Long id, UserEntity userEntity, EventEntity eventEntity) {
+    public RegistrationEntity(
+            Long id,
+            EventEntity event,
+            LocalDateTime dateRegistration
+    ) {
+        this.id = id;
+        this.event = event;
+        this.dateRegistration = dateRegistration;
     }
 
     public RegistrationEntity() {

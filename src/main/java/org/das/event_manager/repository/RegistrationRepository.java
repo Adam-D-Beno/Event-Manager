635e2350dc60576455a374db4a5c98753c9f0db5
ПК_Home <wrofdt@ya.ru>
2025-02-25 20:49:16 +0300

    refactor RegistrationRepository

--- src/main/java/org/das/event_manager/repository/RegistrationRepository.java
+++ src/main/java/org/das/event_manager/repository/RegistrationRepository.java
@@ -1,10 +1,20 @@
 package org.das.event_manager.repository;
 
+import org.das.event_manager.domain.User;
 import org.das.event_manager.domain.entity.RegistrationEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface RegistrationRepository extends JpaRepository<RegistrationEntity, LocationRepository> {
-    List<RegistrationEntity> findAllByUser_Id(Long userId);
+
+    @Query("""
+            SELECT re.event FROM RegistrationEntity re
+                        WHERE re.event.id = :eventId
+            """)
+    Optional<RegistrationEntity> findByEventId(
+            @Param("eventId") Long eventId);
 }

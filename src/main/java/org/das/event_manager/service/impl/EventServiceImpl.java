f4fdba31b3d237387b4e480bd5474f77d343d1d5
ПК_Home <wrofdt@ya.ru>
2025-04-27 17:03:03 +0300

    refactor mappers delete: use lombok

--- src/main/java/org/das/event_manager/service/impl/EventServiceImpl.java
+++ src/main/java/org/das/event_manager/service/impl/EventServiceImpl.java
@@ -14,7 +14,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.validation.annotation.Validated;
 import java.util.List;
 
 @Service

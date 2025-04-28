0318801de53ce59d3c5dfcbd7559bdf6d474e5f7
ПК_Home <wrofdt@ya.ru>
2025-04-28 23:25:40 +0300

    refactor delete config Schedul

--- src/main/java/org/das/event_manager/service/impl/SchedulerService.java
+++ src/main/java/org/das/event_manager/service/impl/SchedulerService.java
@@ -7,6 +7,8 @@ import org.das.event_manager.repository.EventRepository;
 import org.das.event_manager.service.EventService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -14,6 +16,8 @@ import org.springframework.transaction.annotation.Transactional;
 import java.util.List;
 
 @Service
+@EnableScheduling
+@ConditionalOnProperty(name = "scheduler.enabled", matchIfMissing = true)
 public class SchedulerService {
 
     private final static Logger log = LoggerFactory.getLogger(SchedulerService.class);

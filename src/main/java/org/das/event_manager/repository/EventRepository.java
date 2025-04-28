54f4a51c63258c0c9c8f699639570449c882a8bd
ПК_Home <wrofdt@ya.ru>
2025-04-28 21:08:01 +0300

    refactor EventRepository

--- src/main/java/org/das/event_manager/repository/EventRepository.java
+++ src/main/java/org/das/event_manager/repository/EventRepository.java
@@ -1,14 +1,11 @@
 package org.das.event_manager.repository;
 
-import jakarta.validation.constraints.NotNull;
-import org.das.event_manager.domain.Event;
 import org.das.event_manager.domain.EventStatus;
 import org.das.event_manager.domain.entity.EventEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -51,9 +48,9 @@ public  interface EventRepository extends JpaRepository<EventEntity, Long> {
     List<EventEntity> findEventsByOwner_Id(Long ownerId);
 
     @Query("""
-        select ev.id from EventEntity ev 
+        select ev.id from EventEntity ev
         WHERE ev.status = :status
-        and ev.date < CURRENT_TIMESTAMP 
+        and ev.date < CURRENT_TIMESTAMP
     """)
     List<Long> findStartedEventsWithStatus(@Param("status") EventStatus status);
 

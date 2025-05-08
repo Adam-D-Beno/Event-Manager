bf53492b35fc88e8317acf9fd16d83f1f6d3ff36
ПК_Home <wrofdt@ya.ru>
2025-05-08 12:27:15 +0300

    refactor EventController

--- src/main/java/org/das/event_manager/controller/EventController.java
+++ src/main/java/org/das/event_manager/controller/EventController.java
@@ -84,4 +84,24 @@ public class EventController {
         LOGGER.info("Get request for find events creation by user");
         return ResponseEntity.ok().body(eventMapper.toDto(eventService.findAllEventsCreationByOwner()));
     }
+
+    @GetMapping("/test")
+    public ResponseEntity<Void> test() {
+        LOGGER.info("Get request for find events creation by user");
+        EventChangeKafkaMessage changeKafkaMessage = EventChangeKafkaMessage.builder()
+                .eventId(1L)
+                .userEventChangedId(1L)
+                .ownerEventId(1L)
+                .name(new EventFieldChange<>("oldName", "newName"))
+                .MaxPlaces(new EventFieldChange<>(0, 1))
+                .date(new EventFieldChange<>(LocalDateTime.now(), LocalDateTime.now()))
+                .cost(new EventFieldChange<>(BigDecimal.ZERO, BigDecimal.ONE))
+                .duration(new EventFieldChange<>(0, 60))
+                .locationId(new EventFieldChange<>(8L, 10L))
+                .status(new EventFieldChange<>(EventStatus.WAIT_START, EventStatus.STARTED))
+                .userRegistrationsOnEvent(List.of(1L))
+                .build();
+        eventKafkaProducerService.sendEvent(changeKafkaMessage);
+        return ResponseEntity.ok().build();
+    }
 }

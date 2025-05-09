7e943a98f645fedef1840f8190a62daf40c5b7db
ПК_Home <wrofdt@ya.ru>
2025-05-09 11:22:29 +0300

    refactor dateRegistration

--- src/main/java/org/das/event_manager/domain/entity/EventRegistrationEntity.java
+++ src/main/java/org/das/event_manager/domain/entity/EventRegistrationEntity.java
@@ -18,7 +18,7 @@ public class EventRegistrationEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "userId")
+    @Column(name = "user_id")
     private Long userId;
 
     @ManyToOne()
